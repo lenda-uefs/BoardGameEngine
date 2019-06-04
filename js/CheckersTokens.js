07517c4eee383a15640e43c1998771f2ea902b89
Filipe Boaventura <fmbboaventura@gmail.com>
2019-06-04 19:29:34 -0300

    Push dos arquivos de configuração dos tokens para o jogo damas

---
+++ js/CheckersTokens.js
@@ -0,0 +1,27 @@
+exports.tokens = [
+  {positionId: 41, tokenType: "man", ownerId:"Black"},
+  {positionId: 43, tokenType: "man", ownerId:"Black"},
+  {positionId: 45, tokenType: "man", ownerId:"Black"},
+  {positionId: 47, tokenType: "man", ownerId:"Black"},
+  {positionId: 48, tokenType: "man", ownerId:"Black"},
+  {positionId: 50, tokenType: "man", ownerId:"Black"},
+  {positionId: 52, tokenType: "man", ownerId:"Black"},
+  {positionId: 54, tokenType: "man", ownerId:"Black"},
+  {positionId: 57, tokenType: "man", ownerId:"Black"},
+  {positionId: 59, tokenType: "man", ownerId:"Black"},
+  {positionId: 61, tokenType: "man", ownerId:"Black"},
+  {positionId: 63, tokenType: "man", ownerId:"Black"},
+
+  {positionId: 0, tokenType: "man", ownerId:"White"},
+  {positionId: 2, tokenType: "man", ownerId:"White"},
+  {positionId: 4, tokenType: "man", ownerId:"White"},
+  {positionId: 6, tokenType: "man", ownerId:"White"},
+  {positionId: 9, tokenType: "man", ownerId:"White"},
+  {positionId: 11, tokenType: "man", ownerId:"White"},
+  {positionId: 13, tokenType: "man", ownerId:"White"},
+  {positionId: 15, tokenType: "man", ownerId:"White"},
+  {positionId: 16, tokenType: "man", ownerId:"White"},
+  {positionId: 18, tokenType: "man", ownerId:"White"},
+  {positionId: 20, tokenType: "man", ownerId:"White"},
+  {positionId: 22, tokenType: "man", ownerId:"White"}
+]
---
+++ js/TestTokens1.js
@@ -0,0 +1,7 @@
+exports.tokens = [
+  {positionId: 59, tokenType: "man", ownerId:"Black"},
+  {positionId: 50, tokenType: "man", ownerId:"White"},
+  {positionId: 34, tokenType: "man", ownerId:"White"},
+  {positionId: 18, tokenType: "man", ownerId:"White"},
+  {positionId: 52, tokenType: "man", ownerId:"White"}
+]
