59d45d271b7746432f872446e0d9289e085be037
Filipe Boaventura <fmbboaventura@gmail.com>
2019-06-05 19:43:40 -0300

    Script para extrair métricas de código

---
+++ analyzeCode.js
@@ -0,0 +1,20 @@
+var plato = require('plato');
+
+var files = [
+  'game.html',
+  './js/BoardGameAdapter.js',
+  './js/Ludo.js',
+  './js/Checkers.js',
+];
+
+var outputDir = './plato_out';
+// null options for this example
+var options = {
+  title: 'Analizyng...'
+};
+
+var callback = function (report){
+  console.log("Done!");
+};
+
+plato.inspect(files, outputDir, options, callback);
